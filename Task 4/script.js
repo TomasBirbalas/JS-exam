470265257c51e0b87f348f2872f29aa6ca3938eb
tomasbir <tomas.birbalas@gmail.com>
2022-02-23 19:12:05 +0200

    Finished Task 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,38 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "./cars.json";
+
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then((cars) => {
+    renderCards(cars);
+  });
+
+const renderCards = (cars) => {
+  const output = document.querySelector("#output");
+  output.innerHTML = "";
+
+  cars.forEach((car) => {
+    const carCard = createCarCard(car);
+    output.append(carCard);
+  });
+};
+
+const createCarCard = (car) => {
+  const carCard = document.createElement("div");
+  const title = document.createElement("h2");
+  const ul = document.createElement("ul");
+
+  title.innerText = car.brand;
+  car.models.forEach((model) => {
+    const li = document.createElement("li");
+    li.innerText = model;
+    ul.append(li);
+  });
+
+  carCard.setAttribute("class", "card");
+  carCard.append(title, ul);
+
+  return carCard;
+};
