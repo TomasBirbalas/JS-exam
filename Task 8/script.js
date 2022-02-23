ef8904c9fc75f908eba30ef01f9d8cee5353a986
tomasbir <tomas.birbalas@gmail.com>
2022-02-23 19:52:11 +0200

    Finished Task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator(numberA, numberB) {
+  this.numberA = numberA;
+  this.numberB = numberB;
+
+  this.sum = () => {
+    return numberA + numberB;
+  };
+  this.subtraction = () => {
+    return numberA - numberB;
+  };
+  this.multiplication = () => {
+    return numberA * numberB;
+  };
+  this.division = () => {
+    return numberA / numberB;
+  };
+}
+
+const numbers = new Calculator(6, 4);
+console.log("Suma ", numbers.sum());
+console.log("Atimtis ", numbers.subtraction());
+console.log("Daugyba ", numbers.multiplication());
+console.log("Dalyba ", numbers.division());
